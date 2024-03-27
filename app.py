014d953f5925f09a9793905187f0a127e0d448b6
Varun <varunrawat343@gmail.com>
2024-03-27 19:04:16 +0530

    closing price charts

--- app.py
+++ app.py
@@ -22,3 +22,25 @@ df = pdr.get_data_yahoo(y_symbols, start=start_date, end=end_date)
 st.subheader('Data from 2010 - 2019')
 st.write(df.describe())
 
+# Visualizations
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+
+st.subheader('Closing Price vs Time chart with 100MA')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(ma200)
+plt.plot(df.Close)
+st.pyplot(fig)
